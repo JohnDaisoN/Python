a0cb512325d75ed6a9a6629cb3f2143b3a90f4f1
JohnDaisoN <100117753+JohnDaisoN@users.noreply.github.com>
2024-07-17 15:19:15 +0530

    hh

---
+++ interview/NEETCODE/Trees/delnodesandreturnforst.py
@@ -0,0 +1,11 @@
+s = set(to_delete)
+        res = []
+        def dfs(root, flag):
+            if not root: return None
+            toDelete = (root.val in s)
+            root.left = dfs(root.left, toDelete)
+            root.right = dfs(root.right, toDelete)
+            if not toDelete and flag: res.append(root)
+            return None if toDelete else root
+        dfs(root, True)
+        return res
