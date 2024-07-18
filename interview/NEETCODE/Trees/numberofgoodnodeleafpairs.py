1b44b09deb5048f0c0e114b3967ac43973a0bd2c
JohnDaisoN <100117753+JohnDaisoN@users.noreply.github.com>
2024-07-18 20:53:48 +0530

    18-07 python files

---
+++ interview/NEETCODE/Trees/numberofgoodnodeleafpairs.py
@@ -0,0 +1,65 @@
+'''
+1530. Number of Good Leaf Nodes Pairs
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given the root of a binary tree and an integer distance. A pair of two different leaf nodes of a binary tree is said to be good if the length of the shortest path between them is less than or equal to distance.
+
+Return the number of good leaf node pairs in the tree
+'''
+#explanation in mp-5 green bird book
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def countPairs(self, root, distance):
+        """
+        :type root: TreeNode
+        :type distance: int
+        :rtype: int
+        """
+        self.pairs = 0
+
+        def dfs(node):
+            if not node:
+                return defaultdict(int)
+            if not node.left and not node.right:
+                count = defaultdict(int)
+                count[1] = 1
+                return count
+            
+            left_dist = dfs(node.left)
+            right_dist = dfs(node.right)
+
+            for d1 in left_dist:
+                for d2 in right_dist:
+                    if d1+d2 <= distance:
+                        self.pairs += left_dist[d1] * right_dist[d2]
+
+            all_dist = defaultdict(int)
+            for d in left_dist:
+                if d+1 <= distance:
+                    all_dist[d+1] += left_dist[d]
+            for d in right_dist:
+                if d+1 <= distance:
+                    all_dist[d+1] += right_dist[d]
+            return all_dist
+
+        dfs(root)
+        return self.pairs
+
+        
+       
+        
+
+        
+
+
+
+        
